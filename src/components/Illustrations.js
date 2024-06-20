f5b790a81056559c91337ecfe1680ae0eaa25b2f
dev-manish26 <webmanish2603@gmail.com>
2024-06-20 17:37:59 +0530

    alignments

--- src/components/Illustrations.js
+++ src/components/Illustrations.js
@@ -8,15 +8,8 @@ const Illustrations = () => {
     <div className="section bgChange3" id="illustrations" >
       <div className="illustration-content">
         <div className="illustration">illustration</div>
-        <div className="gallery">gallery.</div>
+        <div className="gallery"><span className='gallery-text'>gallery.</span></div>
       </div>
-      <div className="lines-il">
-            <div className="horizontal-line1-il"></div>
-            <div className="horizontal-line2-il"></div>
-            <div className="vertical-line1-il"></div>
-            <div className="vertical-line2-il"></div>
-            <div className="vertical-line3-il"></div>
-          </div>
     </div>
   );
 };
